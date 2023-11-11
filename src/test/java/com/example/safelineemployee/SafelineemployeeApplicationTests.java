f2d5887f04b090aea3c1091d627fb289fc0c237d
Nuray Korganci <nrysnmz@gmail.com>
2023-11-10 19:33:01 -0600

    Entity class added

---
+++ src/test/java/com/example/safelineemployee/SafelineemployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safelineemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafelineemployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
